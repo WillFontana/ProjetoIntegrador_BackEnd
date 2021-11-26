a7aedc0ebc730ee78254ba089c347dae0a31f53c
Rodrigo Sartori Azevedo <rodrigo.sa@edu.unifil.br>
2021-11-25 21:53:58 -0300

    Adiciona entidade Usuário

---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegrador.Models
+{
+    public class Usuario
+    {
+        // id Usuario
+        [Key]
+        public int Id { get; set; }
+        // Senha
+        public string Senha { get; set; }
+        // Crn
+        [Required(ErrorMessage = "O CRN é um campo obrigatório!")]
+        [StringLength(16, ErrorMessage = "O CRN inserido não é valido!")]
+        public string Crn { get; set; }
+    }
+}
